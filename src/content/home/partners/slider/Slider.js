1c0cbf3a71bd732b75d542a994626b1d9b00d648
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2019-08-13 16:32:28 +0300

    Added simple slider

--- src/content/home/partners/slider/Slider.js
+++ src/content/home/partners/slider/Slider.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './Slider.css';
 import SliderItem from './slider_item/SliderItem';
+import provento from './../../../../img/provento.png';
+import phoenix from './../../../../img/phoenix-contact.png';
+import rittal from './../../../../img/rittal_logo.png';
 
 class Slider extends Component {
     render() {
         return (
             <div className="Slider">
-                <SliderItem title="Провенто"/>
-                <SliderItem title="PHOENIX CONTACT"/>
-                <SliderItem title="RITTAL"/>
+                <SliderItem image={provento} title="Провенто"/>
+                <SliderItem image={phoenix} title="PHOENIX CONTACT"/>
+                <SliderItem image={rittal} title="RITTAL"/>
             </div>
         );
     }
--- src/content/home/partners/slider/slider_item/SliderItem.js
+++ src/content/home/partners/slider/slider_item/SliderItem.js
@@ -5,7 +5,10 @@ import './SliderItem.css';
 class SliderItem extends Component {
     render() {
         return (
-            <div className="SliderItem">{this.props.title}</div>
+            <div className="SliderItem">
+                <img src={this.props.image} className="SliderItem-image" alt="logo" />
+                <div className="SliderItem-title">{this.props.title}</div>
+            </div>
         );
     }
 }
