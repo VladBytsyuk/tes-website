cfee1be9062e439f368b5807fb1fdb2122c07df9
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2019-08-18 01:23:33 +0300

    Added Slider controls

--- src/content/home/partners/slider/Slider.js
+++ src/content/home/partners/slider/Slider.js
@@ -4,14 +4,25 @@ import SliderItem from './slider_item/SliderItem';
 import provento from './../../../../img/provento.png';
 import phoenix from './../../../../img/phoenix-contact.png';
 import rittal from './../../../../img/rittal_logo.png';
+import left from './../../../../img/arrow-left.png';
+import right from './../../../../img/arrow-right.png';
 
 class Slider extends Component {
     render() {
         return (
             <div className="Slider">
-                <SliderItem image={provento} title="Провенто"/>
-                <SliderItem image={phoenix} title="PHOENIX CONTACT"/>
-                <SliderItem image={rittal} title="RITTAL"/>
+                <div className="Slider-hbox">
+                    <img src={left} className="Slider-arrow" alt="arrow_left" />
+                    <SliderItem image={provento} title="Провенто"/>
+                    <SliderItem image={phoenix} title="PHOENIX CONTACT"/>
+                    <SliderItem image={rittal} title="RITTAL"/>
+                    <img src={right} className="Slider-arrow" alt="arrow_right" />
+                </div>
+                <div className="Slider-dots-box">
+                    <span className="Slider-dot" />
+                    <span className="Slider-dot" />
+                    <span className="Slider-dot" />
+                </div>
             </div>
         );
     }
